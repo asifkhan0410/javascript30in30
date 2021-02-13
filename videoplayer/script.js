06dbf44c604a04a8fb13be712cb28b86ec2018cf
Asif <asifkhan0410@gmail.com>
2021-02-13 14:57:40 +0530

    added skip functionality

--- videoplayer/script.js
+++ videoplayer/script.js
@@ -16,8 +16,14 @@ function updateButton() {
     const icon = this.paused ? '►' : '❚ ❚';
     console.log(icon);
     toggle.textContent = icon;
-  }
+}
+function skip() {
+    video.currentTime += parseFloat(this.dataset.skip);
+   }
 
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+
+toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
