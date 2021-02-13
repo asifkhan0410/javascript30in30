a06d2eced2804a6b6cc7f6fa44fae464d4be174f
Asif <asifkhan0410@gmail.com>
2021-02-13 15:04:06 +0530

    update range function

--- videoplayer/script.js
+++ videoplayer/script.js
@@ -17,14 +17,19 @@ function updateButton() {
     console.log(icon);
     toggle.textContent = icon;
 }
+
 function skip() {
     video.currentTime += parseFloat(this.dataset.skip);
-   }
+}
 
-   function handleProgress() {
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+}
+
+function handleProgress() {
     const percent = (video.currentTime / video.duration) * 100;
     progressBar.style.flexBasis = `${percent}%`;
-  }
+}
 
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
@@ -33,3 +38,5 @@ video.addEventListener('timeupdate', handleProgress);
 
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
