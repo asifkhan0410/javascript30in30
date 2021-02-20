72738603d55b34385fb85f3a9d83e68b8bf48c0e
Asif <asifkhan0410@gmail.com>
2021-02-20 23:29:00 +0530

    adding manual data

--- geolocation/script.js
+++ geolocation/script.js
@@ -2,9 +2,8 @@ const arrow = document.querySelector('.arrow');
 const speed = document.querySelector('.speed-value');
 
     navigator.geolocation.watchPosition((data) => {
-      console.log(data);
-      speed.textContent = data.coords.speed;
-      arrow.style.transform = `rotate(${data.coords.heading}deg)`;
+      speed.textContent = data.coords.speed || 0;
+      arrow.style.transform = 'rotate(20deg)' || `rotate(${data.coords.heading}deg)`;
     }, (err) => {
       console.error(err);
     });
