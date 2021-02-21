35645f2d305d629698771e20a6a1a1522076bd29
Asif <asifkhan0410@gmail.com>
2021-02-21 11:57:43 +0530

    added text to button

--- bubbling/script.js
+++ bubbling/script.js
@@ -10,3 +10,9 @@ const divs = document.querySelectorAll('div');
     capture: false, //helps to determine whether the capture will be fron inside to outside or vice-versa
     once: true //this allows an element to be clicked once.
   }));
+
+  button.addEventListener('click', () => {
+    console.log('Click!!!');
+  }, {
+    once: true
+  }); // once is used in checkout pages when the user just needs to click once.
