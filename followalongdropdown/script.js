ff5af69ccfc66b1801d856fe454d0452bcf39ac6
Asif <asifkhan0410@gmail.com>
2021-02-21 12:37:23 +0530

    functions done

--- followalongdropdown/script.js
+++ followalongdropdown/script.js
@@ -3,11 +3,14 @@ const triggers = document.querySelectorAll('.cool > li');
   const nav  = document.querySelector('.top');
 
   function handleEnter(){
-
+    this.classList.add('trigger-enter');
+    setTimeout(() => this.classList.contains('trigger-enter') && this.classList.add('trigger-enter-active'), 150);
+    background.classList.add('open');
   }
 
   function handleLeave(){
-
+    this.classList.remove('trigger-enter');
+    background.classList.remove('open');
   }
 
   triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
