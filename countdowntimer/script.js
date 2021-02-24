3d4bdc106d40f8eb7832cfef1b0bb14161a746e6
Asif <asifkhan0410@gmail.com>
2021-02-24 15:48:22 +0530

    changes

--- countdowntimer/script.js
+++ countdowntimer/script.js
@@ -2,26 +2,26 @@ let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
 const buttons = document.querySelectorAll('[data-time]');
-//const startTime = document.querySelector('.timer__start');
+const startTime = document.querySelector('.timer__start');
 
 function timer(seconds){
 
     //clear existing coutdowns
     clearInterval(countdown);
-
+    timerDisplay.removeAttribute('contenteditable','false');
     const now = Date.now();
     const then = now + seconds * 1000;
     displayTimeLeft(seconds);
     displayEndTime(then);
 
-    //startTime.setAttribute('disabled','true');
+    startTime.setAttribute('disabled','true');
     countdown = setInterval(() =>{
         const secondsLeft = Math.round((then - Date.now()) / 1000);
         // check to stop it.
         if(secondsLeft <0){
             clearInterval(countdown);
-            //startTime.removeAttribute('disabled','false');
-            //timerDisplay.innerHTML = "5:00";
+            startTime.removeAttribute('disabled','false');
+            timerDisplay.setAttribute('contenteditable','true');
             return;
         }
         //display time;
@@ -43,13 +43,21 @@ function displayEndTime(timestamp) {
     const hour = end.getHours();
     const adjustedHour = hour > 12 ? hour - 12 : hour;
     const minutes = end.getMinutes();
-    endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes} ${hour>12? 'PM': "AM"}`;
+    endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes} ${hour>12? "PM": "AM"}`;
   }
 
   function startTimer() {
     const seconds = parseInt(this.dataset.time);
     timer(seconds);
   }
+
+  startTime.addEventListener('click', ()=>{
+    const time = timerDisplay.innerHTML;
+    const splittime = time.split(':');
+    const seconds = splittime[0]*60 + Number(splittime[1]);  
+    timer(seconds);  
+});
+
 
 buttons.forEach(button => button.addEventListener('click', startTimer));
 document.customForm.addEventListener('submit', function(e) {
