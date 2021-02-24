50e49eb720524f548e3d2b9744dce3c76788c8eb
Asif <asifkhan0410@gmail.com>
2021-02-24 13:42:37 +0530

    adding button to function countdown time

--- countdowntimer/script.js
+++ countdowntimer/script.js
@@ -1,15 +1,27 @@
-const timerDisplay = document.querySelector('.display__time-left')
+let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
+//const startTime = document.querySelector('.timer__start');
 
 function timer(seconds){
+
+    //clear existing coutdowns
+    clearInterval(countdown);
+
     const now = Date.now();
-    const then = now + seconds*1000;
+    const then = now + seconds * 1000;
     displayTimeLeft(seconds);
-    
-    setInterval(() =>{
-        const secondsLeft = Math.round((then - Date.now())/60);
+    displayEndTime(then);
+
+    //startTime.setAttribute('disabled','true');
+    countdown = setInterval(() =>{
+        const secondsLeft = Math.round((then - Date.now()) / 1000);
         // check to stop it.
         if(secondsLeft <0){
             clearInterval(countdown);
+            //startTime.removeAttribute('disabled','false');
+            //timerDisplay.innerHTML = "5:00";
             return;
         }
         //display time;
@@ -21,7 +33,29 @@ function timer(seconds){
 function displayTimeLeft(seconds){
     const minutes = Math.floor(seconds/60);
     const remainderSeconds = seconds % 60;
-    const display = `${minutes} : ${remainderSeconds<10? '0': ''}${remainderSeconds}`
+    const display = `${minutes}:${remainderSeconds<10? '0': ''}${remainderSeconds}`
     document.title = display;
     timerDisplay.textContent = display;
 }
+
+function displayEndTime(timestamp) {
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const adjustedHour = hour > 12 ? hour - 12 : hour;
+    const minutes = end.getMinutes();
+    endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes} ${hour>12? 'PM': "AM"}`;
+  }
+
+  function startTimer() {
+    const seconds = parseInt(this.dataset.time);
+    timer(seconds);
+  }
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
+document.customForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const mins = this.minutes.value;
+    timer(mins * 60);
+    this.reset();
+  });
+  
