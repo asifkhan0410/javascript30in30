cd8e19426fde1f06c5c5e49f56313abe74254e6e
Asif <asifkhan0410@gmail.com>
2021-02-24 10:40:12 +0530

    display time

--- countdowntimer/script.js
+++ countdowntimer/script.js
@@ -1,14 +1,27 @@
+const timerDisplay = document.querySelector('.display__time-left')
+
 function timer(seconds){
     const now = Date.now();
     const then = now + seconds*1000;
+    displayTimeLeft(seconds);
+    
     setInterval(() =>{
         const secondsLeft = Math.round((then - Date.now())/60);
-
+        // check to stop it.
         if(secondsLeft <0){
             clearInterval(countdown);
             return;
         }
-
+        //display time;
+        displayTimeLeft(secondsLeft);
         
     },1000);
+}
+
+function displayTimeLeft(seconds){
+    const minutes = Math.floor(seconds/60);
+    const remainderSeconds = seconds % 60;
+    const display = `${minutes} : ${remainderSeconds<10? '0': ''}${remainderSeconds}`
+    document.title = display;
+    timerDisplay.textContent = display;
 }
